e5a4f344409b74c7b4bfef3ab8ab0ef2d8b213fc
Joshua Bernstein <joshuatbernstein@gmail.com>
2024-01-11 18:08:33 -0500

    added loop for immediate re-rendering

--- logic/app.js
+++ logic/app.js
@@ -2,12 +2,27 @@ import fs from 'fs';
 import parseFile from './parsing/parser.js';
 // import { encode } from 'html-entities';
 
+async function sleep(seconds){
+    return new Promise((resolve) => {
+        setTimeout(resolve, seconds*1000);
+      });
+}
 
 
-const fileData = fs.readFileSync('input.plm').toString().replaceAll("\r","");
-const resLines = parseFile(fileData);
+const filePath = "input.plm";
+let fileSize = 0;
 
-fs.writeFileSync("out.html",resLines);
+
+while(true){
+    const newSize = fs.statSync(filePath).size;
+    if(fileSize !== newSize){
+        fileSize = newSize;
+        const fileData = fs.readFileSync(filePath).toString().replaceAll("\r","");
+        fs.writeFileSync("out.html",parseFile(fileData));
+    }
+    await sleep(5);
+}
+
 
 
 
